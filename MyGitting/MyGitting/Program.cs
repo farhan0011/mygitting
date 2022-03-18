c6f2ccefb2e12151d8c0d3d68ea36feda04cdb7a
FarhanTanvir_BS0988 <farhan.tanvir@brainstation-23.com>
2022-03-18 11:27:33 +0600

    commit initial

---
+++ MyGitting/MyGitting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGitting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
